655c8b226c52157f3dfa9761787d1d9df1852733
jiaad <abdulziad08@outlook.com>
2019-01-17 23:28:08 +0100

    ajouté les fichiers

--- lib/cryp.rb
+++ lib/cryp.rb
@@ -31,47 +31,7 @@ def hash_combiné
     combined_hash
   end
 end
- hash_combiné
+ p hash_combiné
 
 
 #======================= MARIE ================================================
- def get_town_hall_email(town_hall_urls)
-  email_town_hall = []
-  doc = Nokogiri::HTML(open(town_hall_urls))
-  doc.xpath('//tbody/tr[4]/td[2]').each do |v|
-  email_town_hall = [v.text]
-end
-  return email_town_hall
-
-end
-get_town_hall_email("http://annuaire-des-mairies.com/95/avernes.html")
-
-
-
-def get_townhall_urls
-  doc = Nokogiri::HTML(open("http://annuaire-des-mairies.com/val-d-oise.html"))
-
-  final = []
-  doc.xpath("//tr[2]//p//a/@href").each do |v|
-  final.push(v.text)
-  end
-  return  @result = final.map { |e| e.gsub('./' , 'http://annuaire-des-mairies.com/') }
-end
-
-
-
- def url_email_methode #urls_des_mairies
-   email=[]
-   n = get_townhall_urls.count
-   i = 0
-   while i < n
-     doc = Nokogiri::HTML(open("#{get_townhall_urls[i]}"))
-     doc.xpath('//section[2]/div/table/tbody/tr[4]/td[2]').map do |x|
-        email.push(x.text)
-     end
-     email[i]
-          i += 1
-   end
-
-end
- p url_email_methode
